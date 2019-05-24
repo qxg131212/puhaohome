8254b0e95b150aad12670a1d986578d1899f2489
qxg131212 <qxg131212@163.com>
2019-05-24 18:55:20 +0800

    qxg

---
+++ puhaohome/src/main/java/com/xa/puhaohome/PuhaohomeApplication.java
@@ -0,0 +1,13 @@
+package com.xa.puhaohome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuhaohomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuhaohomeApplication.class, args);
+	}
+
+}
---
+++ puhaohome/src/test/java/com/xa/puhaohome/PuhaohomeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xa.puhaohome;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PuhaohomeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
