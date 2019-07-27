064d22d2266d308d040ed34e39ebaaf600783485
testxiaogang <qxg131212@163.com>
2019-07-27 13:41:55 +0800

    2222

--- puhaohome/src/main/java/com/xa/puhaohome/PuhaohomeApplication.java
+++ puhaohome/src/main/java/com/xa/puhaohome/PuhaohomeApplication.java
@@ -8,6 +8,8 @@ public class PuhaohomeApplication {
 
 	public static void main(String[] args) {
 		System.out.println("这是一份完美的计划；谢谢============================");
+		System.out.println("这是一份完美的计划；谢谢============================");
+		System.out.println("这是一份完美的计划；谢谢============================");
 		SpringApplication.run(PuhaohomeApplication.class, args);
 	}
 
