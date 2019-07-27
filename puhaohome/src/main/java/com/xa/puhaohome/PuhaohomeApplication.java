fbd47a7553decd38a737eec56ba6733bf110930d
testxiaogang <qxg131212@163.com>
2019-07-27 12:20:35 +0800

    乔小刚 main方法测试

--- puhaohome/src/main/java/com/xa/puhaohome/PuhaohomeApplication.java
+++ puhaohome/src/main/java/com/xa/puhaohome/PuhaohomeApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PuhaohomeApplication {
 
 	public static void main(String[] args) {
+
+
+		System.out.println("guanyucishis");
 		SpringApplication.run(PuhaohomeApplication.class, args);
 	}
 
